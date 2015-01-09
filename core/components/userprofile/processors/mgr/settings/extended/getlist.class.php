0e5ca23aa45c95eeab8692bc418a5e2ff2d33d08
Володя Володин <vovanblya@mail.ru>
2015-01-09 14:31:25 +0400

    + [build]

--- core/components/userprofile/processors/mgr/settings/extended/getlist.class.php
+++ core/components/userprofile/processors/mgr/settings/extended/getlist.class.php
@@ -14,33 +14,7 @@ class upExtendedGetListProcessor extends modObjectGetListProcessor {
 
 	/** {@inheritDoc} */
 	public function prepareQueryBeforeCount(xPDOQuery $c) {
-		if ($this->getProperty('combo')) {
-			$c->select('id,name');
-			$c->where(array('active' => 1));
-/*			if ($delivery_id = $this->getProperty('delivery_id')) {
-
-				if ($delivery = $this->modx->getObject('msDelivery', $delivery_id)) {
-					$q = $this->modx->newQuery('msDeliveryMember', array('delivery_id' => $delivery_id));
-					$q->select('payment_id');
-					if ($q->prepare() && $q->stmt->execute()) {
-						$ids = $q->stmt->fetchAll(PDO::FETCH_COLUMN);
-						$c->where(array('id:IN' => $ids));
-					}
-				}
-			}*/
-		}
 		return $c;
-	}
-
-	/** {@inheritDoc} */
-/*	public function prepareQueryBeforeCount(xPDOQuery $c) {
-		return $c;
-	}*/
-
-	/** {@inheritDoc} */
-	public function prepareRow(xPDOObject $object) {
-		$array = $object->toArray();
-		return $array;
 	}
 
 }
