56f151c0d80562cf532923723525c56c38d4da0e
Володя Володин <vovanblya@mail.ru>
2015-01-09 15:39:54 +0400

    + [build]

--- core/components/userprofile/model/userprofile/mysql/upextended.class.php
+++
@@ -1,3 +0,0 @@
-<?php
-require_once (dirname(dirname(__FILE__)) . '/upextended.class.php');
-class upExtended_mysql extends upExtended {}
--- core/components/userprofile/model/userprofile/upextended.class.php
+++
@@ -1,2 +0,0 @@
-<?php
-class upExtended extends xPDOSimpleObject {}
