9244518724426825f3207c6d97228eb186c13c9c
Володя Володин <vovanblya@mail.ru>
2015-01-09 15:43:14 +0400

    + [build]

---
+++ core/components/userprofile/model/userprofile/mysql/upextended.map.inc.php
@@ -0,0 +1,244 @@
+<?php
+$xpdo_meta_map['upExtended']= array (
+  'package' => 'userprofile',
+  'version' => '1.1',
+  'table' => 'up_extended',
+  'extends' => 'xPDOSimpleObject',
+  'fields' => 
+  array (
+    'user_id' => NULL,
+    'type_id' => NULL,
+    'lastname' => NULL,
+    'firstname' => NULL,
+    'secondname' => NULL,
+    'description' => NULL,
+    'introtext' => NULL,
+    'registration' => '0000-00-00 00:00:00',
+    'lastactivity' => '0000-00-00 00:00:00',
+    'ip' => '0.0.0.0',
+  ),
+  'fieldMeta' => 
+  array (
+    'user_id' => 
+    array (
+      'dbtype' => 'int',
+      'precision' => '10',
+      'attributes' => 'unsigned',
+      'phptype' => 'integer',
+      'null' => false,
+    ),
+    'type_id' => 
+    array (
+      'dbtype' => 'int',
+      'precision' => '10',
+      'attributes' => 'unsigned',
+      'phptype' => 'integer',
+      'null' => false,
+    ),
+    'lastname' => 
+    array (
+      'dbtype' => 'varchar',
+      'precision' => '255',
+      'phptype' => 'string',
+      'null' => false,
+    ),
+    'firstname' => 
+    array (
+      'dbtype' => 'varchar',
+      'precision' => '255',
+      'phptype' => 'string',
+      'null' => false,
+    ),
+    'secondname' => 
+    array (
+      'dbtype' => 'varchar',
+      'precision' => '255',
+      'phptype' => 'string',
+      'null' => false,
+    ),
+    'description' => 
+    array (
+      'dbtype' => 'text',
+      'phptype' => 'string',
+      'null' => true,
+    ),
+    'introtext' => 
+    array (
+      'dbtype' => 'text',
+      'phptype' => 'string',
+      'null' => true,
+    ),
+    'registration' => 
+    array (
+      'dbtype' => 'datetime',
+      'phptype' => 'datetime',
+      'null' => true,
+      'default' => '0000-00-00 00:00:00',
+    ),
+    'lastactivity' => 
+    array (
+      'dbtype' => 'datetime',
+      'phptype' => 'datetime',
+      'null' => true,
+      'default' => '0000-00-00 00:00:00',
+    ),
+    'ip' => 
+    array (
+      'dbtype' => 'varchar',
+      'precision' => '16',
+      'phptype' => 'string',
+      'null' => true,
+      'default' => '0.0.0.0',
+    ),
+  ),
+  'indexes' => 
+  array (
+    'user_id' => 
+    array (
+      'alias' => 'user_id',
+      'primary' => false,
+      'unique' => false,
+      'type' => 'BTREE',
+      'columns' => 
+      array (
+        'user_id' => 
+        array (
+          'length' => '',
+          'collation' => 'A',
+          'null' => false,
+        ),
+      ),
+    ),
+    'type_id' => 
+    array (
+      'alias' => 'type_id',
+      'primary' => false,
+      'unique' => false,
+      'type' => 'BTREE',
+      'columns' => 
+      array (
+        'type_id' => 
+        array (
+          'length' => '',
+          'collation' => 'A',
+          'null' => false,
+        ),
+      ),
+    ),
+    'lastname' => 
+    array (
+      'alias' => 'lastname',
+      'primary' => false,
+      'unique' => false,
+      'type' => 'BTREE',
+      'columns' => 
+      array (
+        'lastname' => 
+        array (
+          'length' => '',
+          'collation' => 'A',
+          'null' => false,
+        ),
+      ),
+    ),
+    'firstname' => 
+    array (
+      'alias' => 'firstname',
+      'primary' => false,
+      'unique' => false,
+      'type' => 'BTREE',
+      'columns' => 
+      array (
+        'firstname' => 
+        array (
+          'length' => '',
+          'collation' => 'A',
+          'null' => false,
+        ),
+      ),
+    ),
+    'secondname' => 
+    array (
+      'alias' => 'secondname',
+      'primary' => false,
+      'unique' => false,
+      'type' => 'BTREE',
+      'columns' => 
+      array (
+        'secondname' => 
+        array (
+          'length' => '',
+          'collation' => 'A',
+          'null' => false,
+        ),
+      ),
+    ),
+    'registration' => 
+    array (
+      'alias' => 'registration',
+      'primary' => false,
+      'unique' => false,
+      'type' => 'BTREE',
+      'columns' => 
+      array (
+        'registration' => 
+        array (
+          'length' => '',
+          'collation' => 'A',
+          'null' => false,
+        ),
+      ),
+    ),
+    'lastactivity' => 
+    array (
+      'alias' => 'lastactivity',
+      'primary' => false,
+      'unique' => false,
+      'type' => 'BTREE',
+      'columns' => 
+      array (
+        'lastactivity' => 
+        array (
+          'length' => '',
+          'collation' => 'A',
+          'null' => false,
+        ),
+      ),
+    ),
+    'ip' => 
+    array (
+      'alias' => 'ip',
+      'primary' => false,
+      'unique' => false,
+      'type' => 'BTREE',
+      'columns' => 
+      array (
+        'ip' => 
+        array (
+          'length' => '',
+          'collation' => 'A',
+          'null' => false,
+        ),
+      ),
+    ),
+  ),
+  'aggregates' => 
+  array (
+    'User' => 
+    array (
+      'class' => 'modUser',
+      'local' => 'user_id',
+      'foreign' => 'id',
+      'cardinality' => 'one',
+      'owner' => 'foreign',
+    ),
+    'UserProfile' => 
+    array (
+      'class' => 'modUserProfile',
+      'local' => 'user_id',
+      'foreign' => 'internalKey',
+      'owner' => 'foreign',
+      'cardinality' => 'one',
+    ),
+  ),
+);
