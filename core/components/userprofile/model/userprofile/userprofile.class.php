f992c0147dacdfd5b3038058d7f86eac0ed25552
Володя Володин <vovanblya@mail.ru>
2015-01-10 16:57:52 +0400

    + [build]

--- core/components/userprofile/model/userprofile/userprofile.class.php
+++ core/components/userprofile/model/userprofile/userprofile.class.php
@@ -45,6 +45,15 @@ class userprofile
 			'processorsPath' => $corePath . 'processors/',
 
 			'json_response' => 0,
+			'dateFormat' => 'd F Y, H:i',
+			'dateNow' => 10,
+			'dateDay' => 'day H:i',
+			'dateMinutes' => 59,
+			'dateHours' => 10,
+
+			'gravatarUrl' => 'https://www.gravatar.com/avatar/',
+			'gravatarSize' => 300,
+			'gravatarIcon' => 'mm',
 
 		), $config);
 
@@ -88,13 +97,15 @@ class userprofile
 		$up_extended = array();
 		$profile = $user->getOne('Profile')->toArray();
 		$profile = array_merge($profile, array(
-				'gravatar' => 'http://www.gravatar.com/avatar/' . md5(strtolower($profile['email'])) . '?s=300',
+				'gravatar' => $this->config['gravatarUrl'] . md5(strtolower($profile['email'])) . '?s='.$this->config['gravatarSize'] . '&d=' . $this->config['gravatarIcon'],
 
 			)
 		);
 		if ($upExtended = $this->modx->getObject('upExtended', array('user_id' => $id))) {
 			$up_extended = $upExtended->toArray();
 		}
+		// если extended пуст
+		if (!is_array($profile['extended'])) $profile['extended'] = array();
 		$up_extended = array_merge($profile['extended'], array(
 			'real' => $up_extended,
 			'personal' => $up_extended,
@@ -166,14 +177,15 @@ class userprofile
 	 */
 	public function OnUserSave($sp)
 	{
-		//if (!$this->isNew($sp)) return;
-
+		if (!$this->isNew($sp)) return;
 		$user = $sp['user'];
-		$up_extended = array();
-		$profile = $user->getOne('Profile')->toArray();
-
-		$this->modx->log(1, print_r($profile, 1));
-
+		$userArr = $user->toArray();
+		$id = $userArr['id'];
+		if (!$upExtended = $this->modx->getObject('upExtended', array('user_id' => $id))) {
+			$upExtended = $this->modx->newObject('upExtended', array('user_id' => $id));
+		}
+		$upExtended->set('registration', date('Y-m-d H:i:s'));
+		$upExtended->save();
 	}
 
 	/**
@@ -181,7 +193,15 @@ class userprofile
 	 */
 	public function OnLoadWebDocument($sp)
 	{
-
+		if ($this->modx->user->isAuthenticated($this->modx->context->get('key'))) {
+			$id = $this->modx->user->id;
+			if (!$upExtended = $this->modx->getObject('upExtended', array('user_id' => $id))) {
+				$upExtended = $this->modx->newObject('upExtended', array('user_id' => $id));
+			}
+			$upExtended->set('lastactivity', date('Y-m-d H:i:s'));
+			$upExtended->set('ip', $this->modx->request->getClientIp()['ip']);
+			$upExtended->save();
+		}
 	}
 
 
@@ -231,6 +251,117 @@ class userprofile
 			return true;
 		}
 		return false;
+	}
+
+	/**
+	 * Formats date to "10 minutes ago" or "Yesterday in 22:10"
+	 * This algorithm taken from https://github.com/livestreet/livestreet/blob/7a6039b21c326acf03c956772325e1398801c5fe/engine/modules/viewer/plugs/function.date_format.php
+	 * @param string $date Timestamp to format
+	 * @param string $dateFormat
+	 *
+	 * @return string
+	 */
+	public function dateFormat($date, $dateFormat = null) {
+		$date = preg_match('/^\d+$/',$date) ?  $date : strtotime($date);
+		$dateFormat = !empty($dateFormat) ? $dateFormat : $this->config['dateFormat'];
+		$current = time();
+		$delta = $current - $date;
+		if ($this->config['dateNow']) {
+			if ($delta < $this->config['dateNow']) {return $this->modx->lexicon('ticket_date_now');}
+		}
+		if ($this->config['dateMinutes']) {
+			$minutes = round(($delta) / 60);
+			if ($minutes < $this->config['dateMinutes']) {
+				if ($minutes > 0) {
+					return $this->declension($minutes, $this->modx->lexicon('ticket_date_minutes_back',array('minutes' => $minutes)));
+				}
+				else {
+					return $this->modx->lexicon('ticket_date_minutes_back_less');
+				}
+			}
+		}
+		if ($this->config['dateHours']) {
+			$hours = round(($delta) / 3600);
+			if ($hours < $this->config['dateHours']) {
+				if ($hours > 0) {
+					return $this->declension($hours, $this->modx->lexicon('ticket_date_hours_back',array('hours' => $hours)));
+				}
+				else {
+					return $this->modx->lexicon('ticket_date_hours_back_less');
+				}
+			}
+		}
+		if ($this->config['dateDay']) {
+			switch(date('Y-m-d', $date)) {
+				case date('Y-m-d'):
+					$day = $this->modx->lexicon('ticket_date_today');
+					break;
+				case date('Y-m-d', mktime(0, 0, 0, date('m')  , date('d')-1, date('Y')) ):
+					$day = $this->modx->lexicon('ticket_date_yesterday');
+					break;
+				case date('Y-m-d', mktime(0, 0, 0, date('m')  , date('d')+1, date('Y')) ):
+					$day = $this->modx->lexicon('ticket_date_tomorrow');
+					break;
+				default: $day = null;
+			}
+			if($day) {
+				$format = str_replace("day",preg_replace("#(\w{1})#",'\\\${1}',$day),$this->config['dateDay']);
+				return date($format,$date);
+			}
+		}
+		$m = date("n", $date);
+		$month_arr = $this->modx->fromJSON($this->modx->lexicon('ticket_date_months'));
+		$month = $month_arr[$m - 1];
+		$format = preg_replace("~(?<!\\\\)F~U", preg_replace('~(\w{1})~u','\\\${1}', $month), $dateFormat);
+		return date($format ,$date);
+	}
+
+	/**
+	 * Declension of words
+	 * This algorithm taken from https://github.com/livestreet/livestreet/blob/eca10c0186c8174b774a2125d8af3760e1c34825/engine/modules/viewer/plugs/modifier.declension.php
+	 *
+	 * @param int $count
+	 * @param string $forms
+	 * @param string $lang
+	 *
+	 * @return string
+	 */
+	public function declension($count, $forms, $lang = null) {
+		if (empty($lang)) {
+			$lang = $this->modx->getOption('cultureKey',null,'en');
+		}
+		$forms = $this->modx->fromJSON($forms);
+		if ($lang == 'ru') {
+			$mod100 = $count % 100;
+			switch ($count%10) {
+				case 1:
+					if ($mod100 == 11) {$text = $forms[2];}
+					else {$text = $forms[0];}
+					break;
+				case 2:
+				case 3:
+				case 4:
+					if (($mod100 > 10) && ($mod100 < 20)) {$text = $forms[2];}
+					else {$text = $forms[1];}
+					break;
+				case 5:
+				case 6:
+				case 7:
+				case 8:
+				case 9:
+				case 0:
+				default: $text = $forms[2];
+			}
+		}
+		else {
+			if ($count == 1) {
+				$text = $forms[0];
+			}
+			else {
+				$text = $forms[1];
+			}
+		}
+		return $text;
 	}
 
 }
