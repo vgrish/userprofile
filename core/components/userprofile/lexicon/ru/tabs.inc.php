6de5aec4ffbe804433fc4b55b3138541b1b72043
Володя Володин <vovanblya@mail.ru>
2015-01-10 11:00:51 +0400

    + [build]

--- core/components/userprofile/lexicon/ru/tabs.inc.php
+++ core/components/userprofile/lexicon/ru/tabs.inc.php
@@ -4,5 +4,19 @@ $_lang['up_activity'] = 'Активность';
 $_lang['up_personal'] = 'Личное';
 $_lang['up_social'] = 'Социальные сети';
 
+
+$_lang['up_field_registration'] = 'Регистрации';
+$_lang['up_field_lastactivity'] = 'Активность';
+$_lang['up_field_ip'] = 'Ip адрес';
+
 $_lang['up_field_lastname'] = 'Фамилия';
 $_lang['up_field_firstname'] = 'Имя';
+$_lang['up_field_secondname'] = 'Отчество';
+
+$_lang['up_field_facebook'] = 'Facebook';
+$_lang['up_field_vk'] = 'Вконтакте';
+$_lang['up_field_odnoklassniki'] = 'Одноклассники';
+$_lang['up_field_mail'] = 'Мой мир';
+$_lang['up_field_twitter'] = 'Твиттер';
+
+
