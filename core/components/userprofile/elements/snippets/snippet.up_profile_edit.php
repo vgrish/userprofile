e199ebe0c27251bbd3ab4473d2d1f6b39d5d394e
Володя Володин <vovanblya@mail.ru>
2015-01-13 16:32:58 +0400

    add snippet upProfile [build]

--- core/components/userprofile/elements/snippets/snippet.up_profile_edit.php
+++ core/components/userprofile/elements/snippets/snippet.up_profile_edit.php
@@ -36,4 +36,9 @@ $modx->regClientScript(str_replace('[[+assetsUrl]]', $up->config['assetsUrl'], $
 //print_r($up->config['assetsUrl']);die;
 
 
+$key = md5($modx->toJSON($up->config));
+$_SESSION['upform'][$key] = $up->config;
+$output = str_ireplace('</form>', "\n<input type=\"hidden\" name=\"form_key\" value=\"{$key}\" class=\"disable-sisyphus\" />\n</form>", $output);
+
+
 return $output;
