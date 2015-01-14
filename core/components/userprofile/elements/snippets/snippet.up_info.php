2544e31bafccabcaca9af5c7d8d66daeee380a7a
Володя Володин <vovanblya@mail.ru>
2015-01-14 17:23:17 +0400

    add snippet upProfile [build]

--- core/components/userprofile/elements/snippets/snippet.up_info.php
+++ core/components/userprofile/elements/snippets/snippet.up_info.php
@@ -11,13 +11,10 @@ if(empty($user_id) && $isAuthenticated) {$user_id = $modx->user->id;}
 elseif(empty($user_id)) {
 	if(empty($tplNoUserInfo)) {return $modx->lexicon('up_get_user_err');}
 	$output = empty($tplNoUserInfo)
-		? $up->pdoTools->getChunk('', $scriptProperties) 
+		? $up->pdoTools->getChunk('', $scriptProperties)
 		: $up->pdoTools->getChunk($tplNoUserInfo, $scriptProperties, $up->pdoTools->config['fastMode']);
-	if (!empty($toPlaceholder)) {
-		$modx->setPlaceholder($toPlaceholder, $output);
-	} else {
-		return $output;
-	}
+	if (!empty($toPlaceholder)) {$modx->setPlaceholder($toPlaceholder, $output);}
+	else {return $output;}
 }
 // default properties
 $default = array(
